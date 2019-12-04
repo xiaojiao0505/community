26c9520c3edf722a8b9f135f18f49438be5f9817
xiao <=>
2019-12-04 20:20:15 +0800

    first

---
+++ src/test/java/com/xiaojian/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaojian.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
